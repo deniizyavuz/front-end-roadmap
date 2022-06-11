3022248609e70786cfb9740c0b023bcaec5947ea
denizyavuz99 <denizyavuz.99@gmail.com>
2022-06-11 15:26:23 +0300

    js dom fonksiyonları

---
+++ js-frontend/innerHTML.js
@@ -0,0 +1,10 @@
+let item = document.getElementById('item');
+
+    // create new li element
+    let li = document.createElement('li');
+    li.textContent = 'About Us';
+    // add it to the ul element
+    item.appendChild(li);
+
+    console.log(item.innerHTML);
+
