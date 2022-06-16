200ff8cb70f9d1d9718a235d50fe0d80e8cd653a
denizyavuz99 <denizyavuz.99@gmail.com>
2022-06-17 01:57:35 +0300

    fetch örnekleri eklendi-json

---
+++ js-frontend/fetch.js
@@ -0,0 +1,18 @@
+fetch("js-frontend/settings.json").then((response) => {
+  console.log(response);
+});
+
+
+fetch("js-frontend/settings.json").then((response) => {
+    return response.json();
+  }
+  ).then(responseJson =>{
+    console.log(responseJson);
+    console.log(responseJson.userName);
+
+  });
+  
+  fetch("https://jsonplaceholder.typicode.com/posts").then(response =>
+    response.json())
+    .then(responseJson => { responseJson.forEach(item =>console.log(item)) 
+    })
